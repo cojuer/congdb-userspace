db82fe1cbf48b99a751583d3a6bdad6d1324d721
Roman Sergeychik <rsergeychik@arccn.ru>
2017-07-21 12:40:57 +0300

    Convert ip to string before print

--- congdb_structs.hpp
+++ congdb_structs.hpp
@@ -1,16 +1,28 @@
 #pragma once
 
+#include <arpa/inet.h>
+
+#include <iostream>
 #include <string>
 #include <vector>
 
 namespace congdb {
+
+static std::string uint_ip_to_str(uint32_t int_ip)
+{
+	char str_ip[16];
+	if (inet_ntop(AF_INET, (uint32_t*)&int_ip, str_ip, 16)) {
+		return std::string(str_ip);
+	}
+	return "Error: could not convert ip from uin32_t";
+}
 
 struct tcp_sock_data {
     uint32_t loc_ip;
     uint32_t rem_ip;
 
 	operator std::string() const {
-		return std::to_string(loc_ip) + "-" + std::to_string(rem_ip);
+		return uint_ip_to_str(loc_ip) + "<->" + uint_ip_to_str(rem_ip);
 	}
 };
 
